3dfcd01097854da4b783068200b313712e082517
Kenneth Wimer <wimer@suse.com>
2026-08-13 10:28:14 +0200

    Removing false push

--- docs/rancher-ds-site/theme.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  var sel=document.getElementById('theme');
-  var saved=localStorage.getItem('rds-theme')||'light';
-  document.documentElement.setAttribute('data-theme',saved);
-  if(sel){sel.value=saved;sel.addEventListener('change',function(){
-    document.documentElement.setAttribute('data-theme',sel.value);
-    localStorage.setItem('rds-theme',sel.value);
-  });}
-})();
